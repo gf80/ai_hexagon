8701f9589b9b433a3628b57d619943b0902015b5
gf80 <slavafomenko0@gmail.com>
2022-10-21 23:35:17 +0300

    stable

--- hexagon.py
+++ hexagon.py
@@ -1,25 +1,13 @@
-pole = """
-  __A______B______C___
-1|     ||     ||     |
- |__♟__||__♟__||__♟__|
-2|     ||     ||     |
- |_____||_____||_____|
-3|     ||     ||     |
- |__♙__||__♙__||__♙__|
-"""
-
-a = """
+print("""
 -|_A_|_B_|_C_| 
 1|_♟_|_♟_|_♟_| 
 2|___|___|___| 
 3|_♙_|_♙_|_♙_| 
-"""
+""")
 
-board = [i for i in a]
-
-logic = [[1,1, 1,],
-        ['','',''],
-        [0, 0, 0 ]]
+logic = [['♟','♟','♟'],
+        ['_','_','_'],
+        ['♙','♙','♙']]
 
 n = 1
 abc = "ABC"
@@ -36,19 +24,30 @@ while True:
             #end1 - индекс строки, end0 - индекс столбца
             end0, end1 = abc.index(end[0]), int(end[1])-1
 
-            if logic[start1][start0] == 0: # Проверка пешки
+            if logic[start1][start0] == '♙': # Проверка пешки
                 if start0-end0 == 0: #Ход прямо
-                    if logic[end1][end0] == '' and abs(start1-end1) < 2: #Если клетка свободна и ход правильный
+                    if logic[end1][end0] == '_' and abs(start1-end1) < 2: #Если клетка свободна и ход правильный
                         logic[start1][start0], logic[end1][end0] = logic[end1][end0], logic[start1][start0]
-                        print(logic)
+                        print(f"""
+-|_A_|_B_|_C_| 
+1|_{logic[0][0]}_|_{logic[0][1]}_|_{logic[0][2]}_| 
+2|_{logic[1][0]}_|_{logic[1][1]}_|_{logic[1][2]}_| 
+3|_{logic[2][0]}_|_{logic[2][1]}_|_{logic[2][2]}_| 
+""")
                         n += 1
                         break
                     else:
                         print("Неверный ход!\n")
                 else: #Диагональный ход
-                    if abs(start0-end0) < 2 and logic[end1][end0] == 1:
-                        logic[start1][start0], logic[end1][end0] = logic[end1][end0], ""
-                        print(logic)
+                    if abs(start0-end0) < 2 and logic[end1][end0] == '♟':
+                        logic[end1][end0], logic[start1][start0] = logic[start1][start0], "_"
+                        print(f"""
+-|_A_|_B_|_C_| 
+1|_{logic[0][0]}_|_{logic[0][1]}_|_{logic[0][2]}_| 
+2|_{logic[1][0]}_|_{logic[1][1]}_|_{logic[1][2]}_| 
+3|_{logic[2][0]}_|_{logic[2][1]}_|_{logic[2][2]}_| 
+""")
+                        n+=1
                         break
                     else:
                         print('Неверный ход!\n')
@@ -63,19 +62,30 @@ while True:
             #end1 - индекс строки, end0 - индекс столбца
             end0, end1 = abc.index(end[0]), int(end[1])-1
             
-            if logic[start1][start0] == 1: # Проверка пешки
+            if logic[start1][start0] == '♟': # Проверка пешки
                 if start0-end0 == 0: #Ход прямо
-                    if logic[end1][end0] == '' and abs(start1-end1) < 2: #Если клетка свободна и ход правильный
+                    if logic[end1][end0] == '_' and abs(start1-end1) < 2: #Если клетка свободна и ход правильный
                         logic[start1][start0], logic[end1][end0] = logic[end1][end0], logic[start1][start0]
-                        print(logic)
+                        print(f"""
+-|_A_|_B_|_C_| 
+1|_{logic[0][0]}_|_{logic[0][1]}_|_{logic[0][2]}_| 
+2|_{logic[1][0]}_|_{logic[1][1]}_|_{logic[1][2]}_| 
+3|_{logic[2][0]}_|_{logic[2][1]}_|_{logic[2][2]}_| 
+""")
                         n += 1
                         break
                     else:
                         print("Неверный ход!\n")
                 else: #Диагональный ход
-                    if abs(start0-end0) < 2 and logic[end1][end0] == 0:
-                        logic[end1][end0], logic[start1][start0] = logic[start1][start0], ""
-                        print(logic)
+                    if abs(start0-end0) < 2 and logic[end1][end0] == '♙':
+                        logic[end1][end0], logic[start1][start0] = logic[start1][start0], "_"
+                        print(f"""
+-|_A_|_B_|_C_| 
+1|_{logic[0][0]}_|_{logic[0][1]}_|_{logic[0][2]}_| 
+2|_{logic[1][0]}_|_{logic[1][1]}_|_{logic[1][2]}_| 
+3|_{logic[2][0]}_|_{logic[2][1]}_|_{logic[2][2]}_| 
+""")                    
+                        n+=1
                         break
                     else:
                         print('Неверный ход!\n')
